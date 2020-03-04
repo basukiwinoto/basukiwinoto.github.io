fb272c21b85650bc0b3bfc2070ca2222ba0944b2
Basuki Winoto <basuki.winoto@gmail.com>
2020-03-03 19:15:44 -0500

    init

---
+++ precache-manifest.55633221165a44c6f8277a00d073c97a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e0f35b46770dbc82a3c420f8dbd63b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eb42da5cd86ca75bf0a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a8b0cd4e7ea93b64b157",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6eb42da5cd86ca75bf0a",
+    "url": "/static/js/2.6eea7ac5.chunk.js"
+  },
+  {
+    "revision": "ba1665840a4211d22917503a2a5544f6",
+    "url": "/static/js/2.6eea7ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b0cd4e7ea93b64b157",
+    "url": "/static/js/main.7069b8dc.chunk.js"
+  },
+  {
+    "revision": "aec874f00dfb3e34a4b8",
+    "url": "/static/js/runtime-main.2d0d66e9.js"
+  },
+  {
+    "revision": "989a66aaca9d311d5ca93f69d3e6696c",
+    "url": "/static/media/undraw_male_avatar_323b.989a66aa.png"
+  }
+]);
